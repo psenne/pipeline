c8913fd9e06497bf94c07822c073474e94dece55
MJP <psenne@mjparker.com>
2019-03-29 12:26:27 -0400

    changed archive dropdown to array.filter

--- src/CandidateComponents/CandidatesTable.js
+++ src/CandidateComponents/CandidatesTable.js
@@ -60,46 +60,42 @@ class CandidatesTable extends Component {
                 {this.props.list
                     .filter(isFiltered(filterByStatus))
                     .filter(isSearched(filterBySearch))
+                    .filter(item => {
+                        return item.info.archived === filter;
+                    })
                     .map(item => {
                         const potential_contracts = item.info.potential_contracts ? item.info.potential_contracts.join(", ") : "";
-                        const viewingCurrent = filter === item.info.archived; //item.info.archived is either "current" or "archived"
                         const company = item.info.company ? `with ${item.info.company}` : "";
                         const current_contract = item.info.current_contract ? `on ${item.info.current_contract}` : "";
+                        const toggleArchive = item.info.archived === "archived" ? "current" : "archived"; // set button text and actions for archive candidate button
 
-                        // set button text and actions for archive candidate button
-                        const toggleArchive = item.info.archived === "archived" ? "current" : "archived";
-                        if (viewingCurrent) {
-                            //show only those candidates whose info.archived matches with archived/current dropdown. could've used the filter property
-                            return (
-                                <Grid.Row columns={2} key={item.key} className={classnames("status-" + item.info.status, "candidate-table-row")} onClick={ev => this.ViewCandidate(ev, item.key)}>
-                                    <Grid.Column textAlign="center" width={1}>
-                                        <MiniToolbar item={item} ArchiveCandidate={ev => this.ArchiveCandidate(ev, item, toggleArchive)} />
-                                    </Grid.Column>
-                                    <Grid.Column width={15}>
-                                        <Header>
-                                            <Header.Content>
-                                                {item.info.firstname} {item.info.lastname}
-                                            </Header.Content>
+                        return (
+                            <Grid.Row columns={2} key={item.key} className={classnames("status-" + item.info.status, "candidate-table-row")} onClick={ev => this.ViewCandidate(ev, item.key)}>
+                                <Grid.Column textAlign="center" width={1}>
+                                    <MiniToolbar item={item} ArchiveCandidate={ev => this.ArchiveCandidate(ev, item, toggleArchive)} />
+                                </Grid.Column>
+                                <Grid.Column width={15}>
+                                    <Header>
+                                        <Header.Content>
+                                            {item.info.firstname} {item.info.lastname}
+                                        </Header.Content>
 
-                                            <Header.Subheader>
-                                                {item.info.level} {item.info.skill} {company} {current_contract}
-                                            </Header.Subheader>
-                                        </Header>
-                                        <div>
-                                            <span className="candidate-table-field">Potential contracts:</span> {potential_contracts}
-                                        </div>
-                                        <div>
-                                            <span className="candidate-table-field">Notes:</span> {item.info.notes}
-                                        </div>
-                                        <div>
-                                            <span className="candidate-table-field">Next steps:</span> {item.info.next_steps}
-                                        </div>
-                                    </Grid.Column>
-                                </Grid.Row>
-                            );
-                        } else {
-                            return false;
-                        }
+                                        <Header.Subheader>
+                                            {item.info.level} {item.info.skill} {company} {current_contract}
+                                        </Header.Subheader>
+                                    </Header>
+                                    <div>
+                                        <span className="candidate-table-field">Potential contracts:</span> {potential_contracts}
+                                    </div>
+                                    <div>
+                                        <span className="candidate-table-field">Notes:</span> {item.info.notes}
+                                    </div>
+                                    <div>
+                                        <span className="candidate-table-field">Next steps:</span> {item.info.next_steps}
+                                    </div>
+                                </Grid.Column>
+                            </Grid.Row>
+                        );
                     })}
             </Grid>
         );
