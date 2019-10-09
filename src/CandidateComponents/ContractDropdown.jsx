b9cc99373432e0898c098680470bd9d9d03fbaf0
MJP <psenne@mjparker.com>
2019-10-09 14:55:47 -0400

    sorted contract dropdown

--- src/CandidateComponents/ContractDropdown.jsx
+++ src/CandidateComponents/ContractDropdown.jsx
@@ -35,9 +35,20 @@ export default class ContractDropdown extends Component {
     render() {
         const { contracts } = this.state;
         const { onChange, contractsoverride = [], ...rest } = this.props;
-        const contractList = contracts.filter(OverrideContracts(contractsoverride)).map(({ key, info: contract }) => {
-            return { key: key, text: contract.name, value: contract.name };
-        });
+        const contractList = contracts
+            .filter(OverrideContracts(contractsoverride))
+            .map(({ key, info: contract }) => {
+                return { key: key, text: contract.name, value: contract.name };
+            })
+            .sort((a, b) => {
+                var comparison = 0;
+                if (a.text > b.text) {
+                    comparison = 1;
+                } else if (a.text < b.text) {
+                    comparison = -1;
+                }
+                return comparison;
+            });
         return <Dropdown {...rest} selectOnBlur={false} options={contractList} onChange={this.onChange} />;
     }
 }
